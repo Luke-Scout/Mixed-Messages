6c4bdb65d887adb866ea1bd1fa7f160ae3788e28
Luke Bates <bates.h.luke@gmail.com>
2023-03-24 17:34:18 -0600

    Finished Debugging and ready to deploy

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function generateNewRandomNumber(num) {
-	return Math.floor(math.random() * num);
+	return Math.floor(Math.random() * num);
 }
-// generates new random number with-in 0-num when called
+
 let messagesObjects = {
 	shouldDo: [
 		"Mow the lawn",
@@ -13,32 +13,38 @@ let messagesObjects = {
 	haveDone: ["Making breakfast", "Meal preping", "Grocery shopping"],
 	wantToDo: ["Go on a cruise", "Go to Rome", "Go to Japan"],
 };
+
 let personalMessage = [];
-for (let i = 0; i < messagesObjects.length; i++) {
-	let messageIndex = messagesObjects[i].length;
-	let messageItem = messagesObjects[i];
+for (let i = 0; i < Object.keys(messagesObjects).length; i++) {
+	let messageIndex = generateNewRandomNumber(messagesObjects[Object.keys(messagesObjects)[i]].length);
+	let messageItem = Object.keys(messagesObjects)[i];
 	switch (messageItem) {
 		case "shouldDo":
 			personalMessage.push(
-				`You should ${messagesObjects[i][messageIndex]} right now.`
+				`You should ${messagesObjects[messageItem][messageIndex]} soon.`
 			);
+			break;
 		case "haveDone":
 			personalMessage.push(
-				`Congrats you finished ${messagesObjects[i][messageIndex]}!`
+				`Congrats you finished ${messagesObjects[messageItem][messageIndex]}!`
 			);
+			break;
 		case "wantToDo":
 			personalMessage.push(
-				`If you keep doing good you can ${messagesObjects[i][messageIndex]}.`
+				`If you keep doing good you can ${messagesObjects[messageItem][messageIndex]}.`
 			);
+			break;
 		default:
 			personalMessage.push(
 				"Either something went wrong, or there is not enough information."
 			);
+			break;
 	}
 }
 
 function formatMessage() {
-	let formattedMessage = personalMessage.join("/ ");
+	let formattedMessage = personalMessage.join(" ");
 	console.log(formattedMessage);
 }
 formatMessage();
+
